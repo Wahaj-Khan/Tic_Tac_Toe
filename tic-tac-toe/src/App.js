2a6df4e262ba58c08a8fbd9f08e5c03485e1b40b
Wahaj-Khan <wahajkhan108@gmail.com>
2023-12-10 23:53:13 +0500

    Update App.js

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,6 +2,17 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const pairs = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+
+  ]
   return (
     <div className="mainContainer">
       <div style={{flexDirection: "row"}}>
